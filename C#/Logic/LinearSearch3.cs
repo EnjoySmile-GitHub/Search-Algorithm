2e8ec2735cacba74e4f5c86f8b81d61d3a950620
EnjoySmile <enjoysmile99999@gmail.com>
2024-01-28 17:27:14 +0900

    C#でサーチの処理実現
    ・線形探索法（リニアサーチ）
    ・線形探索法（番兵をたてる）
    ・線形探索法（break版）

---
+++ C#/Logic/LinearSearch3.cs
@@ -0,0 +1,45 @@
+﻿using C_.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_.Logic
+{
+    internal class LinearSearch3 : LogicSearch
+    {
+        /// <summary>
+        /// 線形探索法（break版）
+        /// </summary>
+        public override void Search()
+        {
+            Person[] person = new Person[]
+            {
+                new Person("Ann", 18),
+                new Person("Rolla", 19),
+                new Person("Nancy", 16),
+                new Person("Eluza", 17),
+                new Person("Juliet", 18),
+                new Person("Candy", 16)
+            };
+
+            string keyName = "Candy";
+            bool searchFlg = false;
+
+            for (int i = 0; i < person.Length; i++)
+            {
+                if (person[i].Name.Equals(keyName))
+                {
+                    Console.WriteLine(person[i].Name + " " + person[i].Age);
+                    searchFlg = true;
+                }
+            }
+
+            if (!searchFlg)
+            {
+                Console.WriteLine("見つかりませんでした。");
+            }
+        }
+    }
+}
---
+++ C#/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using C_.Logic;
+
+const int SEARCH_COUNT = 3;
+
+LogicSearch[] logicSearch = new LogicSearch[SEARCH_COUNT];
+// 線形探索法（リニアサーチ）
+logicSearch[0] = new LinearSearch1();
+// 線形探索法（番兵をたてる）
+logicSearch[1] = new LinearSearch2();
+// 線形探索法（break版）
+logicSearch[2] = new LinearSearch3();
+
+for (int i = 0; i < SEARCH_COUNT; i++)
+{
+    if (logicSearch[i] == null)
+    {
+        continue;
+    }
+
+    logicSearch[i].Search();
+}
